4d510311073359cefeb0d1d40ab77466ea0b6b82
simonbeirouti <hello@simonbeirouti.com>
2022-09-12 21:02:43 +1000

    add jwt and bcrypt to app

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 ma = Marshmallow()
+jwt = JWTManager()
+bcrypt = Bcrypt()
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +15,8 @@ def create_app():
     
     db.init_app(app)
     ma.init_app(app)
+    jwt.init_app(app)
+    bcrypt.init_app(app)
 
     from commands import db_commands
     app.register_blueprint(db_commands)
