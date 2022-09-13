29012be9919433e73ffe9a778c4e6bcb9d43bb13
simonbeirouti <hello@simonbeirouti.com>
2022-09-13 17:57:43 +1000

    consolidate name and add books relation

--- models/authors.py
+++ models/authors.py
@@ -3,7 +3,7 @@ from main import db
 class Author(db.Model):
     __tablename__ = 'authors'
     author_id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(), nullable=False)
-    last_name = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String(), nullable=False)
     country = db.Column(db.String(), nullable=False)
     dob = db.Column(db.Date(), nullable=False)
+    books = db.relationship('Book', backref='author')
