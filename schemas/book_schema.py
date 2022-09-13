75f189bcd3e4c654d3a0850b769fc78f39b70589
simonbeirouti <hello@simonbeirouti.com>
2022-09-13 17:59:03 +1000

    add author to book as nested information

--- schemas/book_schema.py
+++ schemas/book_schema.py
@@ -1,9 +1,13 @@
 from main import ma
+from marshmallow import fields
+from schemas.author_schema import AuthorSchema
 
 class BookSchema(ma.Schema):
     class Meta:
         ordered = True
-        fields = ("book_id", "title", "genre", "year", "length")
+        fields = ("book_id", "title", "genre", "year", "length", "author_id", "author")
+        load_only = ['author_id', 'book_id']
+    author = fields.Nested(AuthorSchema, only=("name", "country", "dob"))
 
 book_schema = BookSchema()
 books_schema = BookSchema(many=True)
