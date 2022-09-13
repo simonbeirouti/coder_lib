fcd93d68ca125d3a19119d1d97b0e72579f36f8f
simonbeirouti <hello@simonbeirouti.com>
2022-09-13 17:58:24 +1000

    add books info nesting to author

--- schemas/author_schema.py
+++ schemas/author_schema.py
@@ -1,9 +1,12 @@
 from main import ma
+from marshmallow import fields
 
 class AuthorSchema(ma.Schema):
     class Meta:
         ordered = True
-        fields = ("author_id", "first_name", "last_name", "country", "dob")
+        fields = ("author_id", "name", "country", "dob", "books")
+        load_only = ["author_id"]
+    books = fields.List(fields.Nested("BookSchema", only=("title", "genre", "year", "length")))
 
 author_schema = AuthorSchema()
 authors_schema = AuthorSchema(many=True)
