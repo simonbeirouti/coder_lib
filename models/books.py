33a82bbb1c61ebfd58e514b60eacb9ff2dcd64f8
simonbeirouti <hello@simonbeirouti.com>
2022-09-13 17:57:15 +1000

    add FK for author and reservations

--- models/books.py
+++ models/books.py
@@ -7,4 +7,5 @@ class Book(db.Model):
     genre = db.Column(db.String(), nullable=False)
     year = db.Column(db.Date(), nullable=False)
     length = db.Column(db.Integer, nullable=False)
-    # author_id = db.Column(db.Integer, db.ForeignKey('authors.author_id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.author_id'), nullable=False)
+    reservations = db.relationship('Reservation', backref='book', cascade="all, delete")
